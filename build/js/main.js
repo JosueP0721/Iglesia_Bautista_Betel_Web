0014eab0be2dd8b679579ee67e8fffd89ea3e0e1
JosueP0721 <115521291+JosueP0721@users.noreply.github.com>
2023-06-20 23:01:53 -0500

    OPTIM_SCSS_HEADER_NONEDISPLAY

--- build/js/main.js
+++ build/js/main.js
@@ -2,12 +2,12 @@ const menuHamburger = document.querySelector(".navegacion__boton")
 const navLinks = document.querySelector(".header__navegacion")
 
 menuHamburger.addEventListener('click',()  =>{ 
-    if(navLinks.classList.contains('none-display')) {
-        navLinks.classList.remove('none-display');
+    if(navLinks.classList.contains('header__navegacion__none-display')) {
+        navLinks.classList.remove('header__navegacion__none-display');
     } else {
         navLinks.classList.add('none');
         setTimeout(() => {
-            navLinks.classList.add('none-display');
+            navLinks.classList.add('header__navegacion__none-display');
             navLinks.classList.remove('none');
           }, 800);
     }
